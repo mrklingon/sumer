3509a9ce317803a31578d256d3e3e34ea1df5b0f
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-11-24 20:46:17 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 function showStats () {
+    svMode = Mode
+    Mode = 0
     basic.showString("P")
     basic.showNumber(Population)
     basic.showString("G")
     basic.showNumber(Grain)
-}
-function shoPlanting () {
-	
+    basic.showString("F")
+    basic.showNumber(EAT)
+    basic.showString("S")
+    basic.showNumber(Planting)
+    Mode = svMode
 }
 function doYear () {
-	
+    Grain = Grain - Planting
+    Crop = randint(20, 50) / 10 * Planting
+    basic.showString("C")
+    basic.showNumber(Crop)
+    Death = Math.trunc(15 / EAT * randint(5, 20))
+    basic.showString("D")
+    basic.showNumber(Death)
+    Immigrants = Math.trunc(EAT / 15 * randint(5, 10))
+    basic.showString("I")
+    basic.showNumber(Immigrants)
+    Population = Immigrants + (Population - Death)
+    Grain = Grain + Crop
 }
 input.onButtonPressed(Button.A, function () {
     if (Food == Mode) {
@@ -17,16 +32,24 @@ input.onButtonPressed(Button.A, function () {
             EAT = 0
         }
     }
-})
-function shoFood () {
-	
-}
-input.onButtonPressed(Button.AB, function () {
-    Mode += 1
-    if (2 < Mode) {
-        Mode = 0
+    if (Plant == Mode) {
+        Planting += -10
+        if (Planting < 0) {
+            Planting = 0
+        }
     }
-    modes[Mode].showImage(0)
+    if (0 == Mode) {
+        doYear()
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    svMode = Mode
+    Mode = 0
+    svMode += 1
+    if (2 < svMode) {
+        svMode = 0
+    }
+    modes[svMode].showImage(0)
     basic.pause(1000)
     basic.showLeds(`
         . . . . .
@@ -35,6 +58,7 @@ input.onButtonPressed(Button.AB, function () {
         . . . . .
         . . . . .
         `)
+    Mode = svMode
 })
 input.onButtonPressed(Button.B, function () {
     if (Food == Mode) {
@@ -43,14 +67,35 @@ input.onButtonPressed(Button.B, function () {
             EAT = 30
         }
     }
+    if (Plant == Mode) {
+        Planting += 10
+        if (Planting > ACRES) {
+            Planting = ACRES
+        }
+    }
+    if (0 == Mode) {
+        basic.showString("C")
+        basic.showNumber(Crop)
+        basic.showString("D")
+        basic.showNumber(Death)
+        basic.showString("I")
+        basic.showNumber(Immigrants)
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     showStats()
 })
+let Immigrants = 0
+let Death = 0
+let Crop = 0
+let svMode = 0
 let modes: Image[] = []
 let Population = 0
 let Grain = 0
+let Plant = 0
 let Food = 0
+let Planting = 0
+let ACRES = 0
 let EAT = 0
 let Mode = 0
 basic.showString("Sumer")
@@ -70,9 +115,10 @@ basic.showLeds(`
     `)
 Mode = 0
 EAT = 20
-let ACRES = 1000
+ACRES = 1000
+Planting = 500
 Food = 1
-let Plant = 2
+Plant = 2
 Grain = randint(2500, 3000)
 Population = 100
 modes = [images.createImage(`
@@ -95,13 +141,25 @@ modes = [images.createImage(`
     . . # . .
     `)]
 basic.forever(function () {
-	
-})
-basic.forever(function () {
     if (Food == Mode) {
         led.plotBarGraph(
         EAT,
         30
         )
+    }
+    if (Plant == Mode) {
+        led.plotBarGraph(
+        Planting,
+        ACRES
+        )
+    }
+    if (Grain < EAT * Population) {
+        EAT = 0
+    }
+    if (Grain < Planting + EAT * Population) {
+        Planting = Planting - 100
+        if (Planting < 0) {
+            Planting = 0
+        }
     }
 })
